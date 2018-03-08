260839122b4406dc1cab86ae440f1b2c868d42fa
Aayush Tyagi <aayush@Aayushs-MacBook-Pro.local>
2018-03-08 14:56:33 -0800

    added comments

--- app/src/main/java/com/aayush/pokedexsquared/MainActivity.java
+++ app/src/main/java/com/aayush/pokedexsquared/MainActivity.java
@@ -40,6 +40,7 @@ import static com.aayush.pokedexsquared.Utils.*;
 public class MainActivity extends AppCompatActivity {
     ViewPager fragmentPager;
 
+    @SuppressLint("StaticFieldLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,28 +71,51 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * An adapter for our ViewPager (that we named fragmentPager)
+     *
+     * It works just like a RecyclerView Adapter, except it's a bit simpler. I defined it in the
+     * activity, although it's fine to define it in a new file.
+     */
     class PokemonPageAdapter extends FragmentStatePagerAdapter {
         String pokemonName = "diglett";
+
+        //our whole pager will be initialized with the activity's fragment manager, and some pokemon name
         public PokemonPageAdapter(FragmentManager fm, String pokemonName) {
             super(fm);
             this.pokemonName = pokemonName;
         }
 
+        //which fragment for which screen (in this case we can just pass in the index of the screen as an argument
         @Override
         public Fragment getItem(int position) {
             return PokemonFragment.newInstance(pokemonName, position);
         }
 
+        //how many fragments
         @Override
         public int getCount() {
             return 2;
         }
     }
 
+    /**
+     * A fragment that represents *any* fragment in the viewpager. We just pass in the position in the
+     * pager (in this case 0 or 1) and do different things based on that.
+     */
     public static class PokemonFragment extends Fragment {
         int position;
         String pokemonName;
 
+        /**
+         * WE DON'T USE CONSTRUCTORS. We put all the arguments into a bundle, call the EMPTY constructor
+         * (which doesn't do anything) and use the built in set arguments method with the bundle.
+         *
+         * Note that this method is static.
+         * @param pokemonName
+         * @param position
+         * @return the fragment
+         */
         static PokemonFragment newInstance(String pokemonName, int position) {
             PokemonFragment fragment = new PokemonFragment();
             Bundle args = new Bundle();
@@ -102,6 +126,11 @@ public class MainActivity extends AppCompatActivity {
             return fragment;
         }
 
+        /**
+         * This method is called before we inflate the layout, so all we can do here is set the arguments
+         * from the bundle passed in earlier (which we can retrieve with getArguments())
+         * @param savedInstanceState
+         */
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -112,10 +141,15 @@ public class MainActivity extends AppCompatActivity {
         @SuppressLint("StaticFieldLeak")
         @Nullable
         @Override
+        /**
+         * This is where all the work is done. We do different things based on what the position is
+         * in the viewPager. Note that we have to inflate the view, do what we want with it, and return it
+         */
         public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
             final View view;
             switch(position) {
                 case 0:
+                    //this line is pretty much just copy paste, but change the layout
                     view = inflater.inflate(R.layout.fragment_pokemon, container, false);
                     new AsyncTask<Void, Void, JSONObject>() {
 
@@ -136,6 +170,7 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         protected void onPostExecute(JSONObject jsonObject) {
+                            //pretty much the same as earlier, remember to use view.findViewById
                             if (jsonObject != null) {
                                 TextView name, attack, defense, spatk, spdef, hp, speed;
                                 ImageView imageView;
@@ -166,52 +201,55 @@ public class MainActivity extends AppCompatActivity {
 
                     break;
                 default:
+                    //if it's not the first page, inflate this other layout file, which has other content
+                    //never actually filled this page up but you can do whatever you want here, like
+                    //make a get request for the pokemon's moves and fill a recyclerview
                     view = inflater.inflate(R.layout.fragment_moves, container, false);
             }
             return view;
         }
     }
 
-    /**
-     * So this class implements the interface we made in Utils. This is how lambdas / higher
-     * order functions work in Java. As a JSONRunnable, it can be passed into the AsyncTask
-     */
-    class SetPokemonViews implements JSONRunnable {
-        @Override
-        public void run(JSONObject jsonObject) {
-            if (jsonObject != null) {
-                TextView name, attack, defense, spatk, spdef, hp, speed;
-                ImageView imageView;
-                name = findViewById(R.id.textView);
-                attack = findViewById(R.id.attack);
-                defense = findViewById(R.id.defense);
-                spatk = findViewById(R.id.spatk);
-                spdef = findViewById(R.id.spdef);
-                hp = findViewById(R.id.hp);
-                speed = findViewById(R.id.speed);
-                imageView = findViewById(R.id.imageView);
-                try {
-                    name.setText(jsonObject.getString("name"));
-                    JSONArray stats = jsonObject.getJSONArray("stats");
-                    /* Typically Strings are retrieved from the strings.xml file using
-                    * this getString(R.string.afkjald); In this case, we are using a string
-                    * template in the strings.xml file (look at it in res/values/string.xml),
-                    * so we pass in our arguments into getString as well. Android Studio will not
-                    * autocomplete this for you, but it will complain. */
-                    speed.setText(getString(R.string.speed, stats.getJSONObject(0).getInt("base_stat")));
-                    spdef.setText(getString(R.string.spdef, stats.getJSONObject(1).getInt("base_stat")));
-                    spatk.setText(getString(R.string.spatk, stats.getJSONObject(2).getInt("base_stat")));
-                    defense.setText(getString(R.string.defense, stats.getJSONObject(3).getInt("base_stat")));
-                    attack.setText(getString(R.string.attack, stats.getJSONObject(4).getInt("base_stat")));
-                    hp.setText(getString(R.string.hp, stats.getJSONObject(5).getInt("base_stat")));
-                    String imageUrl = jsonObject.getJSONObject("sprites").getString("front_default");
-                    Glide.with(MainActivity.this).load(imageUrl).into(imageView);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                Toast.makeText(MainActivity.this, "Not a pokemon", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
+//    /**
+//     * So this class implements the interface we made in Utils. This is how lambdas / higher
+//     * order functions work in Java. As a JSONRunnable, it can be passed into the AsyncTask
+//     */
+//    class SetPokemonViews implements JSONRunnable {
+//        @Override
+//        public void run(JSONObject jsonObject) {
+//            if (jsonObject != null) {
+//                TextView name, attack, defense, spatk, spdef, hp, speed;
+//                ImageView imageView;
+//                name = findViewById(R.id.textView);
+//                attack = findViewById(R.id.attack);
+//                defense = findViewById(R.id.defense);
+//                spatk = findViewById(R.id.spatk);
+//                spdef = findViewById(R.id.spdef);
+//                hp = findViewById(R.id.hp);
+//                speed = findViewById(R.id.speed);
+//                imageView = findViewById(R.id.imageView);
+//                try {
+//                    name.setText(jsonObject.getString("name"));
+//                    JSONArray stats = jsonObject.getJSONArray("stats");
+//                    /* Typically Strings are retrieved from the strings.xml file using
+//                    * this getString(R.string.afkjald); In this case, we are using a string
+//                    * template in the strings.xml file (look at it in res/values/string.xml),
+//                    * so we pass in our arguments into getString as well. Android Studio will not
+//                    * autocomplete this for you, but it will complain. */
+//                    speed.setText(getString(R.string.speed, stats.getJSONObject(0).getInt("base_stat")));
+//                    spdef.setText(getString(R.string.spdef, stats.getJSONObject(1).getInt("base_stat")));
+//                    spatk.setText(getString(R.string.spatk, stats.getJSONObject(2).getInt("base_stat")));
+//                    defense.setText(getString(R.string.defense, stats.getJSONObject(3).getInt("base_stat")));
+//                    attack.setText(getString(R.string.attack, stats.getJSONObject(4).getInt("base_stat")));
+//                    hp.setText(getString(R.string.hp, stats.getJSONObject(5).getInt("base_stat")));
+//                    String imageUrl = jsonObject.getJSONObject("sprites").getString("front_default");
+//                    Glide.with(MainActivity.this).load(imageUrl).into(imageView);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            } else {
+//                Toast.makeText(MainActivity.this, "Not a pokemon", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 }
--- app/src/main/java/com/aayush/pokedexsquared/Utils.java
+++ app/src/main/java/com/aayush/pokedexsquared/Utils.java
@@ -12,6 +12,10 @@ import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by aayush on 2018-02-24.
@@ -26,74 +30,83 @@ class Utils {
         return s.hasNext() ? s.next() : "";
     }
 
-    /**
-     * Basically a wrapper for some method that takes in a JSONObject and does something
-     * with it. THIS IS NOT AN ACTUAL RUNNABLE. This is just how we handle lambdas in Java.
-     * You implement this interface with a bunch of different classes, but you can always
-     * call the run method, which will come in handy later
-     */
-    interface JSONRunnable {
-        void run(JSONObject jsonObject);
-    }
-
-    /**
-     * AsyncTasks should always be static if they do any UI stuff, otherwise memory leaks can occur.
-     * It looks weird, but it opens more possibilities to organizing your code better.
-     */
-    static class FetchJSONTask extends AsyncTask<Void, Void, JSONObject> {
-        final String urlString;
-
-        /*
-         * This can be any JSONRunnable! Let's say we had some other activity that needed us to
-         * get some weather data and use the JSONObject from the GET request to update a
-         * RecyclerView or something. All we need to do is define a new JSONRunnable whose run
-         * method updates the RecyclerView given a JSONObject, and call
-         *
-         * new FetchJSONTask(urlString, WeatherJSONRunnable r, WeatherActivity.this).exectute()
-         *
-         * No need to copy paste this AsyncTask over and over!
-         */
-        final JSONRunnable r;
-
-        /* Because of the way android handles activities, you shouldn't have them in an AsyncTask
-         * a WeakReference is like a barebones copy that will accomplish what we need */
-        final WeakReference<Activity> activityReference;
-
-        FetchJSONTask(String urlString, JSONRunnable r, Activity activity) {
-            this.urlString = urlString;
-            this.r = r;
-            this.activityReference = new WeakReference<>(activity); //make it a reference
-        }
-
-        @Override
-        protected JSONObject doInBackground(Void... voids) {
-            try {
-                URL url = new URL(urlString);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                InputStream in = new BufferedInputStream(conn.getInputStream());
-                String response = convertStreamToString(in);
-                return new JSONObject(response);
-            } catch (IOException | JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(final JSONObject jsonObject) {
-            /*
-             * Ok so first we're getting the activity from the reference with .get(). It's ok to
-             * use the activity as long as it's not in an instance variable. Then we run our
-             * JSONRunnable's run method with the retrieved JSONObject, making sure it's on the
-             * UI thread with the syntax below.
-             */
-            activityReference.get().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    r.run(jsonObject);
-                }
-            });
-        }
-    }
+//    /**
+//     * Basically a wrapper for some method that takes in a JSONObject and does something
+//     * with it. THIS IS NOT AN ACTUAL RUNNABLE. This is just how we handle lambdas in Java.
+//     * You implement this interface with a bunch of different classes, but you can always
+//     * call the run method, which will come in handy later
+//     */
+//    interface JSONRunnable {
+//        void run(JSONObject jsonObject);
+//    }
+//
+//    public static Future<JSONObject> getJSONObject(final String urlString) {
+//        URL url = new URL(urlString);
+//        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//        conn.setRequestMethod("GET");
+//        InputStream in = new BufferedInputStream(conn.getInputStream());
+//        String response = convertStreamToString(in);
+//        return new JSONObject(response);
+//    }
+//
+//    /**
+//     * AsyncTasks should always be static if they do any UI stuff, otherwise memory leaks can occur.
+//     * It looks weird, but it opens more possibilities to organizing your code better.
+//     */
+//    static class FetchJSONTask extends AsyncTask<Void, Void, JSONObject> {
+//        final String urlString;
+//
+//        /*
+//         * This can be any JSONRunnable! Let's say we had some other activity that needed us to
+//         * get some weather data and use the JSONObject from the GET request to update a
+//         * RecyclerView or something. All we need to do is define a new JSONRunnable whose run
+//         * method updates the RecyclerView given a JSONObject, and call
+//         *
+//         * new FetchJSONTask(urlString, WeatherJSONRunnable r, WeatherActivity.this).exectute()
+//         *
+//         * No need to copy paste this AsyncTask over and over!
+//         */
+//        final JSONRunnable r;
+//
+//        /* Because of the way android handles activities, you shouldn't have them in an AsyncTask
+//         * a WeakReference is like a barebones copy that will accomplish what we need */
+//        final WeakReference<Activity> activityReference;
+//
+//        FetchJSONTask(String urlString, JSONRunnable r, Activity activity) {
+//            this.urlString = urlString;
+//            this.r = r;
+//            this.activityReference = new WeakReference<>(activity); //make it a reference
+//        }
+//
+//        @Override
+//        protected JSONObject doInBackground(Void... voids) {
+//            try {
+//                URL url = new URL(urlString);
+//                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                conn.setRequestMethod("GET");
+//                InputStream in = new BufferedInputStream(conn.getInputStream());
+//                String response = convertStreamToString(in);
+//                return new JSONObject(response);
+//            } catch (IOException | JSONException e) {
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(final JSONObject jsonObject) {
+//            /*
+//             * Ok so first we're getting the activity from the reference with .get(). It's ok to
+//             * use the activity as long as it's not in an instance variable. Then we run our
+//             * JSONRunnable's run method with the retrieved JSONObject, making sure it's on the
+//             * UI thread with the syntax below.
+//             */
+//            activityReference.get().runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    r.run(jsonObject);
+//                }
+//            });
+//        }
+//    }
 }
